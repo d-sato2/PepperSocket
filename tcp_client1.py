59c60959789d307842893531a715f8b5ed23d7fc
daisuke <daisuke.satou2@e-harp.jp>
2016-04-14 17:30:05 +0900

    first commit

---
+++ tcp_client1.py
@@ -0,0 +1,11 @@
+import socket
+
+host = "192.168.1.179"
+port = 1234
+bufsize = 4096
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, port))
+sock.send("test")
+sock.close()
+pass
