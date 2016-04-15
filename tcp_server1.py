5e86afd1bee249410cda6645683f0318f30f4b2d
daisuke <daisuke.satou2@e-harp.jp>
2016-04-15 10:31:40 +0900

    PCとPepper間でのデータ送受信時刻をやり取りできるようにした

--- tcp_server1.py
+++ tcp_server1.py
@@ -1,12 +1,16 @@
 from __future__ import print_function
 import socket
 from contextlib import closing
+import datetime
 
 def main():
   host = '192.168.1.179'
   port = 4000
   backlog = 10
   bufsize = 4096
+  now = datetime.datetime.now()
+  mili = now.strftime("%Y%m%d%H%M%S.") + "%04d" % (now.microsecond // 1000)
+  print (mili)
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   with closing(sock):
@@ -17,7 +21,10 @@ def main():
       with closing(conn):
         msg = conn.recv(bufsize)
         print(msg)
-        conn.send(msg)
+        now = datetime.datetime.now()
+        mili = now.strftime("%Y%m%d%H%M%S.") + "%04d" % (now.microsecond // 1000)
+        print("PCtime:" + mili)
+        conn.send("PCtime:" + mili)
   return
 
 if __name__ == '__main__':
